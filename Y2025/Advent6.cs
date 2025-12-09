659eea8405ba417e022a74b74fd58c2f3264c8fd
d <d@e>
2025-12-09 18:03:48 +1100

    Little work on 6, 7

--- Y2025/Advent6.cs
+++ Y2025/Advent6.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Advent.A2024;
 
 namespace Advent.Y2025
 {
@@ -16,25 +17,54 @@ namespace Advent.Y2025
             }
             string[] lines = file.Split(new string[]{"\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
+            long[][] numbers = lines.Take(lines.Length - 1).Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray()).ToArray();
+            char[] ops = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(c => c.Single()).ToArray();
+
             Stopwatch st1 = new Stopwatch();
             st1.Start();
-            task1();
+            task1(numbers, ops);
             st1.Stop();
 
 
             Stopwatch st2 = new Stopwatch();
             st2.Start();
-            task2();
+            task2(numbers, ops);
             st2.Stop();
 
             Console.WriteLine($"Time 1: {st1.ElapsedMilliseconds}, Time 2: {st2.ElapsedMilliseconds}" );
 
         }
 
-        private static void task1() {
+        static Func<long, long, long> mult = delegate (long a, long b)
+        {
+            return a * b;
+        };
+
+        private static void task1(long[][] numbers, char[] ops)
+        {
+            List<long> results = new List<long>();
+            for (int i=0; i < numbers[0].Length; i++)
+            {
+                if (ops[i] == '+')
+                {
+                    results.Add(numbers.Select(Line => Line[i]).Sum());
+                }
+                else if (ops[i] == '*')
+                {
+                    results.Add(numbers.Select(Line => Line[i]).Aggregate((a, b) => a * b));
+                    // numbers.Select(Line => Line[i]).Aggregate(delegate(long a, long b)
+                    // {
+                    //     return a * b;
+                    // });
+                    // 
+                    //numbers.Select(Line => Line[i]).Aggregate(mult);
+                }
+                
+            }
+            Console.WriteLine("Result: " + results.Sum());
         }
 
-        private static void task2() {
+        private static void task2(long[][] numbers, char[] ops) {
         }
 
     }
--- Y2025/Advent7.cs
+++ Y2025/Advent7.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Advent.lib;
+using System.Runtime.CompilerServices;
 
 namespace Advent.Y2025
 {
@@ -15,6 +17,14 @@ namespace Advent.Y2025
                 file = sr.ReadToEnd();
             }
             string[] lines = file.Split(new string[]{"\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            Grid<ManifoldEntry> g = new Grid<ManifoldEntry>(lines.Select(l => l.Select(HardCodedConverter.Factory(
+                ('^', ManifoldEntry.Splitter),
+                ('S', ManifoldEntry.Start),
+                ('.', ManifoldEntry.Empty)
+            ))));
+
 
             Stopwatch st1 = new Stopwatch();
             st1.Start();
@@ -31,10 +41,130 @@ namespace Advent.Y2025
 
         }
 
+        public enum ManifoldEntry
+        {
+            Start,
+            Splitter,
+            Empty
+        }
+
         private static void task1() {
         }
 
         private static void task2() {
+        }
+
+        // public class CharConverter<T>
+        // {
+        //     Func<char, T> getDefault;
+        //     Dictionary<char, Func<char, T>> mapping;
+
+        //     private CharConverter(Func<char, T> getDefault, Dictionary<char, Func<char, T>> mapping)
+        //     {
+        //         this.getDefault = getDefault;
+        //         this.mapping = mapping;
+        //     }
+
+        //     public static CharConverter<T> Factory(params (char, T)[] map)
+        //     {
+        //         return Factory(default(T), map);
+        //     }
+
+        //     public static CharConverter<T> Factory(T defaultValue, params (char, T)[] conversions)
+        //     {
+        //         // if (defaultValue.GetType() != typeof(T))
+        //         // {
+        //         //     throw new Exception("Default value type must match declared return type " + typeof(T).ToString());
+        //         // }
+
+        //         // if (!conversions.All(c => c.Item2.GetType() == typeof(T)))
+        //         // {
+        //         //     throw new Exception("Type of all values in map must match declared return type " + typeof(T).ToString());
+        //         // }
+
+        //         Func<char, T> getDefault = delegate (char c){return (T)defaultValue;};
+        //         Dictionary<char, Func<char, T>> getMapping = 
+        //             conversions.ToDictionary<(char, T), char, Func<char, T>>(item => item.Item1, item => delegate(char c) { return (T)item.Item2; });
+        //             //conversions.ToDictionary<(char, T), char, Func<char, T>>(item => item.Item1, item => c => item.Item2 );
+
+        //         return new CharConverter<T>(getDefault, getMapping);
+        //     }
+
+        //     public static CharConverter<T> Factory(Func<char, T> defaultValue, params (char, Func<char, T>)[] conversions)
+        //     {
+        //         return new CharConverter<T>(defaultValue, conversions.ToDictionary(item => item.Item1, item => item.Item2));
+        //     }
+
+        //     public static CharConverter<T> Factory(T defaultValue, params (char, Func<char, T>)[] conversions)
+        //     {
+        //         return Factory((c) => defaultValue, conversions);
+        //     }
+
+        //     // public CharConverter(T defaultValue, params (char, object)[] conversions)
+        //     // {
+        //     //     this.defaultValue = defaultValue;
+        //     //     this.conversions = conversions.ToDictionary(t => t.Item1, t => t.Item2);
+        //     // }
+
+        //     public T Convert(char c)
+        //     {
+        //         Func<char, T> val;
+        //         if (mapping.TryGetValue(c, out val))
+        //         {
+        //             return val(c);
+        //         }
+        //         return this.getDefault(c);
+        //     }
+        // }
+
+        public class HardCodedConverter
+        {
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(Func<TItem, TConverter> defaultValue, Dictionary<TItem, Func<TItem, TConverter>> conversions)
+            {
+                return delegate (TItem c)
+                {
+                    Func<TItem, TConverter> getDefault = defaultValue;
+                    Dictionary<TItem, Func<TItem, TConverter>> mapping = conversions;
+
+                    Func<TItem, TConverter> val;
+                    if (mapping.TryGetValue(c, out val))
+                    {
+                        return val(c);
+                    }
+                    return getDefault(c);
+                };
+
+            }
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(params (TItem, TConverter)[] map)
+            {
+                return Factory(default, map);
+            }
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(TConverter defaultValue, params (TItem, TConverter)[] conversions)
+            {
+                Func<TItem, TConverter> getDefault = delegate (TItem c){return (TConverter)defaultValue;};
+                Dictionary<TItem, Func<TItem, TConverter>> getMapping = 
+                    conversions.ToDictionary<(TItem, TConverter), TItem, Func<TItem, TConverter>>(item => item.Item1, item => delegate(TItem c) { return (TConverter)item.Item2; });
+
+                return Factory(getDefault, getMapping);
+            }
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(Func<TItem, TConverter> defaultValue, params (TItem, Func<TItem, TConverter>)[] conversions)
+            {
+                return Factory(defaultValue, conversions.ToDictionary(item => item.Item1, item => item.Item2));
+            }
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(TConverter defaultValue, params (TItem, Func<TItem, TConverter>)[] conversions)
+            {
+                return Factory((c) => defaultValue, conversions);
+            }
+
+            public static Func<TItem, TConverter> Factory<TItem, TConverter>(params (TItem, Func<TItem, TConverter>)[] conversions)
+            {
+                return Factory((c) => default, conversions);
+            }
         }
 
     }
